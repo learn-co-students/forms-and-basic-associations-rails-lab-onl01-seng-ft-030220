e787f42594c0f133d45bd28dab7cb775a8644889
pharia-le <pharia.le@gmail.com>
2020-05-17 08:20:31 -0700

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,15 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def genre_name=(name)
+    self.genre = Genre.find(name: name)
+  end
+
+  def genre_name
+    self.genre ? self.genre : nil
+  end
+
 end
